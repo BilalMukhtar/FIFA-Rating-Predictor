25716ee3674ab6af6ffc66922602bbf7595ecbe9
BilalMukhtar <bmanutd511@gmail.com>
2023-07-13 19:40:13 -0400

    updation

--- sql files/inserts.sql
+++ sql files/inserts.sql
@@ -4,4 +4,10 @@ JOIN PLAYER_STATS ps ON p.player_id = ps.player_id
 JOIN PLAYER_RATINGS pr ON p.player_id = pr.player_id
 WHERE ps.season = 22 and pr.season = 23;
 
-SELECT * FROM PLAYER
+
+SELECT *, *, curr.*, prevr.*
+FROM PLAYER p
+JOIN PLAYER_STATS ps ON p.player_id = ps.player_id
+JOIN PLAYER_RATINGS prevr ON p.player_id = prevr.player_id
+JOIN PLAYER_RATINGS curr ON p.player_id = curr.player_id
+WHERE ps.season = prevr.season AND ps.season = curr.season-1;
